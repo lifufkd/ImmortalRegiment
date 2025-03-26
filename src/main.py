f416ac5a2cf3f785e6f194599cefb28194928369
SBR <dmsemen45@gmail.com>
2025-03-26 21:35:00 +0300

    Add new exceptions handlers

--- src/main.py
+++ src/main.py
@@ -1,17 +1,32 @@
+import asyncio
 from fastapi import FastAPI, status
 from fastapi.responses import JSONResponse
 from contextlib import asynccontextmanager
+from fastapi_pagination import add_pagination
 
 from src.repository.init_db import create_tables, create_schema
 from src.routes.heroes import heroes_router
-from src.utilities.exceptions_storage import HeroNotFound, WarNotFound, MilitaryRankNotFound, HeroOnModeration
+from src.utilities.exceptions_storage import (
+    HeroNotFound,
+    WarNotFound,
+    MilitaryRankNotFound,
+    HeroOnModeration,
+    InvalidFileType,
+    FIleToBig,
+    FileNotFound,
+    ImageCorrupted
+)
 from src.storage.local import FileManager
+from src.triggers.triggers import setup_hero_delete_trigger
+from src.triggers.listeners import setup_user_delete_listener
 
 
 @asynccontextmanager
 async def lifespan(_: FastAPI):
     await create_schema()
     await create_tables()
+    await setup_hero_delete_trigger()
+    asyncio.create_task(setup_user_delete_listener())
     FileManager.create_folders_structure()
     yield
 
@@ -47,6 +62,27 @@ async def exception_handler(request, exc: Exception) -> JSONResponse:
                 status_code=status.HTTP_404_NOT_FOUND,
                 content={"detail": str(exc)}
             )
+        case InvalidFileType():
+            return JSONResponse(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                content={"detail": str(exc)},
+            )
+        case FIleToBig():
+            return JSONResponse(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                content={"detail": str(exc)},
+            )
+        case ImageCorrupted():
+            return JSONResponse(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                content={"detail": str(exc)},
+            )
+        case FileNotFound():
+            return JSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND,
+                content={"detail": str(exc)}
+            )
 
 app.include_router(heroes_router)
 
+add_pagination(app)
