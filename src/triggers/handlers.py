8810a1a84cf4834a77d8180d077a210dcd47b708
SBR <dmsemen45@gmail.com>
2025-03-28 20:30:08 +0300

    Fix JSON unpack error

--- src/triggers/handlers.py
+++ src/triggers/handlers.py
@@ -5,13 +5,11 @@ from src.utilities.config import generic_settings
 from src.broker.redis import RedisBroker
 
 
-async def handle_hero_insert_changes(payload: str):
-    hero_id = json.loads(payload)
-    await RedisBroker().broker_client.publish("hero:hero_insert_events", str(hero_id))
+async def handle_hero_insert_changes(hero_id: str):
+    await RedisBroker().broker_client.publish("hero:hero_insert_events", hero_id)
 
 
-async def handle_hero_delete_changes(payload: str):
-    photo_name = json.loads(payload)
+async def handle_hero_delete_changes(photo_name: str):
 
     file_manager = FileManager()
     filepath = generic_settings.MEDIA_FOLDER / f"{photo_name}"
