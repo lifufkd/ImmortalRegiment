47faa4a8af6cea54c12daeef0a0953c864692e7d
lilfufkd <dmsemen45@gmail.com>
2025-03-28 16:11:08 +0300

    Add trigger for insert events

--- src/triggers/handlers.py
+++ src/triggers/handlers.py
@@ -2,14 +2,21 @@ import json
 
 from src.storage.local import FileManager
 from src.utilities.config import generic_settings
+from src.broker.redis import RedisBroker
+
+
+async def handle_hero_insert_changes(payload: str):
+    hero_id = json.loads(payload)
+    await RedisBroker().broker_client.publish("hero:hero_insert_events", str(hero_id))
 
 
 async def handle_hero_delete_changes(payload: str):
-    row_data = json.loads(payload)
-    file_manager = FileManager()
-    filepath = generic_settings.MEDIA_FOLDER / f"{row_data.get('photo_name')}"
+    photo_name = json.loads(payload)
 
-    if row_data.get('photo_name') is None:
+    file_manager = FileManager()
+    filepath = generic_settings.MEDIA_FOLDER / f"{photo_name}"
+
+    if photo_name is None:
         return None
     if not (await file_manager.file_exists(file_path=filepath)):
         return None
